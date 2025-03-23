5c7d0458d7dbbfc459a28731ddd8f6729c574c48
Deniel Moises <116956562+DMTolibas@users.noreply.github.com>
2025-03-24 04:44:39 +0800

    make the update funct

--- anime_recom/anime_list/urls.py
+++ anime_recom/anime_list/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="home"),
+    path('delete-anime/<str:pk>', views.deleteanime, name="delete-anime"), #the str: pk get the id of the obj and send it to the f(x) in view
+    path('update-anime/<str:pk>', views.updateanime, name="update-anime"), #the str: pk get the id of the obj and send it to the views.updateanime
+
     path('recommendation', views.recommend, name="recommendation"),
 ]
--- anime_recom/anime_list/views.py
+++ anime_recom/anime_list/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Anime
@@ -25,3 +25,34 @@ def index(request):
 
 def recommend(request):
     return render(request,'recom.html')
+
+def deleteanime(request, pk):
+    anime = get_object_or_404(Anime, id=pk)      # get the object that the user is req, but if not found, show 404 error
+    # anime = Anime.objects.get(id=pk) // you can also try this one
+    if request.method == 'POST':
+        anime.delete()
+
+        return redirect('home')
+
+    context = {'anime': anime}
+    return render(request, 'delete-anime.html', context) 
+
+
+def updateanime(request, pk):
+    anime = get_object_or_404(Anime, id=pk) #get the obj.
+
+    form = AnimeForm(instance=anime)   #This code set the data that we request as the place holder//instance = whatever data we get/request
+
+    if request.method == 'POST':
+
+        form = AnimeForm(request.POST, instance=anime) #change the current data into new data
+
+        if form.is_valid():
+
+            form.save()
+        
+            return redirect('home')
+        
+    context = {'AnimeForm': form}
+
+    return render(request, 'update-anime.html', context)
